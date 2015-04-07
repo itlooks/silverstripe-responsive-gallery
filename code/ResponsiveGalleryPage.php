030c8f1e253ddde0000aa2ee57fa51c299613987
ChristophHauff <hauff@itlooks.de>
2015-04-07 13:34:40 +0200

    Removed public static $allowed_actions from controller

--- code/ResponsiveGalleryPage.php
+++ code/ResponsiveGalleryPage.php
@@ -50,23 +50,10 @@ class ResponsiveGalleryPage extends Page {
 
 class ResponsiveGalleryPage_Controller extends Page_Controller {
 
-	  /**
-	   * An array of actions that can be accessed via a request. Each array element should be an action name, and the
-	   * permissions or conditions required to allow the user to access it.
-	   *
-	   * <code>
-	   * array (
-	   *     'action', // anyone can access this action
-	   *     'action' => true, // same as above
-	   *     'action' => 'ADMIN', // you must have ADMIN permissions to access this action
-	   *     'action' => '->checkAction' // you can only access this action if $this->checkAction() returns true
-	   * );
-	   * </code>
-	   *
-	   * @var array
-	   */
-	  public static $allowed_actions = array ();
-
+    /**
+     * Get requirements of a single responsive gallery object, id is needed to create unique html boxes to handle
+     *  multiple galleries on a single page. 
+     */
 	  public function init() {
           parent::init();
           ResponsiveGalleryExtension::getRequirements($this->ID);
