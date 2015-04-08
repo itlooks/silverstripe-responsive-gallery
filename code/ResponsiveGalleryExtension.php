a1ca601daf0259ad0059780092473892057cb90a
ChristophHauff <hauff@itlooks.de>
2015-04-09 00:15:23 +0200

    Add an alternative image selection method, which enables admins to choose a folder that contains all desired images of a gallery

--- code/ResponsiveGalleryExtension.php
+++ code/ResponsiveGalleryExtension.php
@@ -5,21 +5,32 @@ class ResponsiveGalleryExtension extends DataExtension {
     /**
      * @var array
      */
+    public static $db = array(
+        'Source' => 'Varchar(2)',
+    );
+
+    /**
+     * @var array
+     */
     public static $has_one = array(
-        'UploadFolder' => 'Folder'
+        'UploadFolder' => 'Folder',
+        'SourceFolder' => 'Folder',
     );
 
     /**
      * @var array
      */
     public static $many_many = array(
-        'GalleryImages' => 'ResponsiveGalleryImage'
+        'GalleryImages' => 'ResponsiveGalleryImage',
     );
 
+    /**
+     * @var array
+     */
     static $many_many_extraFields = array( 
-	    'GalleryImages' => array( 
-		    'SortOrder' => "Int" 
-	    )
+	      'GalleryImages' => array( 
+		        'SortOrder' => "Int",
+	      )
     );
 
     /**
@@ -53,48 +64,182 @@ JS
 
     /**
      * Standard function to generate cms-form
+     *
+     * @param \FieldSet
      * 
-     * @return FieldSet
+     * @return \FieldSet
      */
     public function updateCMSFields(FieldList $fields) {
         parent::updateCMSFields($fields);
 
         // create assets/responsive-gallery folder if not exists
         Folder::find_or_make('responsive-gallery');
-        $folderObject = Folder::find('responsive-gallery');
+
+        $aFields = array();
+
+        $aFields[] = new HeaderField(
+            "Choose your desired image source"
+        );
+
+        $aFields[] = new OptionsetField(
+            "Source",
+            "Source",
+            array(
+                "sf" => "Source Folder",
+                "dl" => "Data List",
+            ),
+            "sf"
+        );
 
         if ($this->owner->ID > 0) {
-            $gridFieldConfig = GridFieldConfig_RelationEditor::create()->addComponents(
-                new GridFieldEditButton(),
-                new GridFieldDeleteAction(),
-                new GridFieldDetailForm(),
-                new GridFieldBulkUpload('GalleryImage'),
-                new GridFieldSortableRows('SortOrder')
-            );
+            switch($this->owner->Source) {
+                case "dl":
+                    $oGridFieldConfig = GridFieldConfig_RelationEditor::create()->addComponents(
+                        new GridFieldEditButton(),
+                        new GridFieldDeleteAction(),
+                        new GridFieldDetailForm(),
+                        new GridFieldBulkUpload('GalleryImage'),
+                        new GridFieldSortableRows('SortOrder')
+                    );
 
-            $gridFieldConfig->getComponentByType('GridFieldBulkUpload')
-                ->setUfSetup('setFolderName', $this->getUploadFolder())
-                ->setUfConfig('sequentialUploads', true);
+                    $oGridFieldConfig->getComponentByType('GridFieldBulkUpload')
+                        ->setUfSetup('setFolderName', $this->getUploadFolder())
+                        ->setUfConfig('sequentialUploads', true);
 
-            $fields->addFieldsToTab(
-                'Root.Gallery',
-                array(
-                    $folderField = new TreeDropdownField(
+                    $aFields[] = new HeaderField(
+                        _t(
+                            'ResponsiveGallery.DATALIST_HEADER',
+                            'Create your image list'
+                        )
+                    );
+
+                    $aFields[] = LiteralField::create(
+                        "DataListInfo",
+                        '<div class="field"><p>'.
+                        _t(
+                            'ResponsiveGallery.DATALIST_INFO',
+                            'You can select images from files or upload images and add them to your customized image list. '.
+                            'Use "Target Folder" field to select a default target folder for your image uploads.'
+                        ).
+                        '</p></div>'
+                    );
+
+                    $aFields[] = new TreeDropdownField(
                         'UploadFolderID',
-                        _t('ResponsiveGallery.FOLDER_LABEL', 'Target Folder'),
+                        _t('ResponsiveGallery.UPLOADFOLDER_LABEL', 'Target Folder'),
                         'Folder'
-                    ),
-                    new GridField(
+                    );
+
+                    $aFields[] = new GridField(
                         "GalleryImages",
                         _t('ResponsiveGallery.IMAGES_LABEL', 'Images'),
                         $this->sortedGalleryImages(),
-                        $gridFieldConfig
-                    )
-                )
-            );
+                        $oGridFieldConfig
+                    );
+
+                    break;
+
+                case "sf":
+                    $iImageCount = $this->countImages();
+
+                    $aFields[] = new HeaderField(
+                        _t(
+                            'ResponsiveGallery.SOURCEFOLDER_HEADER',
+                            'Select source folder of gallery'
+                        )
+                    );
+
+                    $aFields[] = new TreeDropdownField(
+                        'SourceFolderID',
+                        _t('ResponsiveGallery.SOURCEFOLDER_LABEL', 'Source Folder'),
+                        'Folder'
+                    );
+                    
+                    if($this->isSourcefolderSelected()) {
+                        $aFields[] = LiteralField::create(
+                            "ImageCountInfo",
+                            '<div class="field">'.
+                            '<p class="info-message">'.
+                            _t(
+                                'ResponsiveGallery.IMAGECOUNTINFO',
+                                'There are {imageCount} images in your selected folder.',
+                                'The number of images in this gallery',
+                                array('imageCount' => $iImageCount)
+                            ).
+                            '</p></div>'
+                        );
+                    } else {
+                        $aFields[] = LiteralField::create(
+                            "NoSelectedFolderInfo",
+                            '<div class="field">'.
+                            '<p><span class="info-message">'.
+                            _t(
+                                'ResponsiveGallery.NOSELECTEDFOLDERINFO',
+                                'Please select a folder that contains the images to be displayed in this gallery.'
+                            ).
+                            '</p></div>'
+                        );
+                    }
+
+                    break;
+                
+                default:
+                    $aFields[] = LiteralField::create(
+                        "SelectSourceInfo",
+                        '<div class="field">'.
+                        '<p><span class="info-message">'.
+                        _t(
+                            'ResponsiveGallery.SELECTSOURCEINFO_HEADER',
+                            'Please select your desired image source type in field above.'
+                        ).
+                        '</span><br/>'.
+                        _t(
+                            'ResponsiveGallery.SELECTSOURCEINFO',
+                            'Then click on save button below, to be able to configure this gallery.'
+                        ).
+                        '</p></div>'
+                    );
+            }
         }
+        
+        $fields->findOrMakeTab('Root.Gallery');
+        $fields->addFieldsToTab(
+            'Root.Gallery',
+            $aFields
+        );
 
         return $fields;
+    }
+
+    /**
+     * Get a sorted set of image objects
+     *
+     * @return \DataList | \ManyManyList
+     */
+    public function getImages() {
+        if($this->owner->Source == "sf") {
+            return DataObject::get("Image", "ParentID = '".$this->owner->SourceFolderID."'");
+        } else {
+            return $this->owner->GalleryImages();
+        }
+    }
+
+    /**
+     * Return true, if an image source folder is selected, false otherwise
+     *
+     * @return bool
+     */
+    public function isSourcefolderSelected() {
+        return ($this->owner->SourceFolderID > 0);
+    }
+
+    /**
+     * Return the number of items in this gallery
+     *
+     * @return int 
+     */
+    public function countImages() {
+        return $this->getImages()->count();
     }
 
     /**
@@ -103,33 +248,14 @@ JS
      * @return string
      */
     public function getUploadFolder() {
-        if ($this->useDefaultUploadFolder()) {
+        if ($this->owner->UploadFolder()->ID == 0) {
             return "responsive-gallery";
         }
-        
-        $uploadFolder = $this->owner->UploadFolder()->Filename;
-        $uploadFolder = str_replace("assets/", "", $uploadFolder);
-        return $uploadFolder;
-    }
 
-    /**
-     * Should uploaded pictures be moved into default folder "responsive-gallery"
-     * Returns true, if pictures should be uploaded into default folder
-     *
-     * @return bool
-     */
-    public function useDefaultUploadFolder() {
-        return ($this->owner->UploadFolder()->ID == 0);
-    }
-
-    /**
-     * Get a sorted set of image objects
-     *
-     * @return \DataObjectList
-     */
-    public function sortedGalleryImages() {
-        $galleryImages = $this->owner->GalleryImages();
-
-        return $galleryImages;
+        return str_replace(
+            "assets/",
+            "",
+            $this->owner->UploadFolder()->Filename
+        );
     }
 }
